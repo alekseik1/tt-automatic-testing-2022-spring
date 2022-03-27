a8f731c230a8e3afed801e17b4e3f5e035451237
alekseik1 <1alekseik1@gmail.com>
2022-03-27 14:50:23 +0300

    test: init

---
+++ tests/hw1/test_basic.py
@@ -0,0 +1,20 @@
+import pytest
+
+from tests.hw1.base import BaseCase
+from tests.ui.locators import basic_locators
+
+
+class TestExample(BaseCase):
+    @pytest.mark.parametrize("query", [pytest.param("pycon"), pytest.param("python")])
+    def test_search(self, query):
+        self.search(query)
+        assert "No results found" not in self.driver.page_source
+
+    @pytest.mark.xfail("some failure")
+    def test_negative_search(self):
+        self.search("adasdasdasdasdasda")
+        assert "No results found" not in self.driver.page_source
+
+    @pytest.mark.UI
+    def test_page_change(self):
+        self.click(basic_locators.GO_BUTTON_LOCATOR)
---
+++ tests/ui/locators/basic_locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+QUERY_LOCATOR = (By.NAME, "q")
+QUERY_LOCATOR_ID = (By.ID, "id-search-field")
+GO_BUTTON_LOCATOR = (By.XPATH, '//*[@id="submit"]')
