e2269cf4b3649272532bdcd2b3e1d9d2eac5587b
alekseik1 <1alekseik1@gmail.com>
2022-03-27 18:57:40 +0300

    fix: mark UI

--- tests/test_start_page.py
+++ tests/test_start_page.py
@@ -1,7 +1,10 @@
+import pytest
+
 from tests.ui.locators.start import StartPageLocators
 from tests.ui.pages.base import BasePage
 
 
+@pytest.mark.UI
 def test_can_find_button(driver):
     page = BasePage(driver)
     page.click(StartPageLocators.LOGIN_BUTTON)
